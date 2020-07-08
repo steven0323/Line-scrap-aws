cdc93cd91f89edef14ad34b9435c52295bc6fdec
yu hsuan <Steph199603@gmail.com>
2020-07-08 16:17:47 +0800

    second

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.set_headless(headless=True)
-driver = webdriver.Firefox(firefox_options=options, executable_path='/home/ubuntu/geckodriver')
+driver = webdriver.Firefox(firefox_options=options, executable_path='/home/ubuntu/Line-scrap-aws/geckodriver')
 driver.get("http://www.imdb.com/title/tt3783958/")
 elem = driver.find_element_by_css_selector('strong span')
 print("Rating: {}".format(elem.text))
